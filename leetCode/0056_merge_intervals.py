e5e2462ce26148a6b225ca1c515802482bd65864
Samuel Lee <leesamuel423@gmail.com>
2024-02-20 17:57:29 -0500

    complete

---
+++ leetCode/0056_merge_intervals.py
@@ -0,0 +1,48 @@
+# 56. Merge Intervals
+"""
+Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+
+ 
+
+Example 1:
+
+Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+Example 2:
+
+Input: intervals = [[1,4],[4,5]]
+Output: [[1,5]]
+Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+ 
+
+Constraints:
+
+1 <= intervals.length <= 104
+intervals[i].length == 2
+0 <= starti <= endi <= 104
+"""
+
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals = sorted(intervals, key=lambda x: x[0])
+        final = []
+        for el in intervals:
+            if not final or final[-1][1] < el[0]:
+                final.append(el)
+            elif final[-1][1] < el[1]:
+                final[-1][1] = el[1]
+        return final
+
+
+"""
+sort first
+final = []
+iterate through intervals
+  if final is empty or index 1 of last value in final is less than index 0 of el
+    append to final
+  else:
+    replace index 1 of last val in final with index 1 of el
+return final
+"""
---
+++ leetCode/0560_subarray_sum_equals_k.py
@@ -0,0 +1,54 @@
+# 560. Subarray Sum Equals K
+"""
+Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1], k = 2
+Output: 2
+Example 2:
+
+Input: nums = [1,2,3], k = 3
+Output: 2
+ 
+
+Constraints:
+
+1 <= nums.length <= 2 * 104
+-1000 <= nums[i] <= 1000
+-107 <= k <= 107
+"""
+from collections import defaultdict
+
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        final = 0
+        prefix = defaultdict(int)
+        prefix[0] = 1
+        sum = 0
+        for i in nums:
+            sum += i
+            if sum - k in prefix:
+                final += prefix[sum - k]
+            prefix[sum] += 1
+        return final
+
+
+"""
+[1, 2, 3]
+       i
+prefix = {
+  0:1
+}
+sum = 0
+final = 0
+iterate through nums
+  add to sum
+  if sum - k exists in prefix, add to final
+  add to prefix
+"""
